a196ad4a0e168f2e4758330e5a6c7693bed5d4ec
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-03-23 00:29:10 +0330

    feat: init test

---
+++ src/main/java/dev/mohrez/Calculator.java
@@ -0,0 +1,4 @@
+package dev.mohrez;
+
+public class Calculator {
+}
---
+++ src/test/java/dev/mohrez/CalculatorTest.java
@@ -0,0 +1,11 @@
+package dev.mohrez;
+
+import org.junit.Test;
+
+class CalculatorTest {
+
+    @Test()
+    public void test(){
+
+    }
+}
