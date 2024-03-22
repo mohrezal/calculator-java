3eaec1fe60a0ed1f26da9384b0aec21bcfdb747c
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-03-23 01:07:07 +0330

    feat: add test to add method

--- src/main/java/dev/mohrez/calculator/Calculator.java
+++ src/main/java/dev/mohrez/calculator/Calculator.java
@@ -3,6 +3,6 @@ package dev.mohrez.calculator;
 public class Calculator extends AbstractCalculator {
     @Override
     public double add(double num1, double num2) {
-        return 0;
+        return num1+num2;
     }
 }
--- src/test/java/dev/mohrez/CalculatorTest.java
+++ src/test/java/dev/mohrez/CalculatorTest.java
@@ -1,11 +1,32 @@
 package dev.mohrez;
 
+import dev.mohrez.calculator.Calculator;
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-class CalculatorTest {
+public class CalculatorTest {
+
+    private Calculator calculator = null;
+    private final double DELTA = 0.0001;
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
 
     @Test()
-    public void test(){
-
+    public void additionTwoPositiveDoubleNumbers(){
+       double result =  calculator.add(2.5,3);
+        assertEquals(5.5,result,DELTA);
+    }
+    @Test()
+    public void  additionTwoNegativeDoubleNumbers(){
+        double result = calculator.add(-1.87,-2);
+        assertEquals(-3.87,result,DELTA);
+    }
+    @Test()
+    public void additionWithZeroNumber(){
+        double result =  calculator.add(1.0,0);
+        assertEquals(1.0,result,DELTA);
     }
 }
